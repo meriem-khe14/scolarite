8056072f9c48a3528f0c57a81379ad79ce36749f
meriem-khe14 <mimkhe6@gmail.com>
2026-04-10 16:16:44 +0100

    Description des modifications

--- test.php
+++ test.php
@@ -1,13 +1,5 @@
 <?php
-require_once 'db.php';
-
-$result = $conn->query("SELECT * FROM etudiant");
-
-if ($result && $result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        echo " ID: " . $row['id'] . " - Nom: " . $row['nom'] . "<br>";
-    }
-} else {
-    echo " Aucun étudiant trouvé.";
-}
+// dashboard_etudiant.php
+session_start();
+echo "✅ Connecté ! Bienvenue " . $_SESSION['user_nom'];
 ?>
